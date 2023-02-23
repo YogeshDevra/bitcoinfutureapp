32d463da76a01847d43d7b6fe68fa74097964d03
Pranav Ratusaria <pranavratusaria6130@gmail.com>
2023-02-23 18:18:18 +0530

    Updated_Code

--- lib/loser.dart
+++ lib/loser.dart
@@ -176,146 +176,147 @@ class _TopLoser extends State<TopLoser> with SingleTickerProviderStateMixin{
 
   Widget topLooser(){
     var list = topLooserAndGainerList.where((element) => double.parse(element.diffRate!)< 0).toList();
+
     return ListView.builder(
         shrinkWrap: true,
         physics: const ScrollPhysics(),
         itemCount: list.length,
         itemBuilder: (BuildContext context, int i) {
           return InkWell(
-            child: Column(
-              children: [
-                Container(
-                  color: Colors.white,
-                  child: Padding(
-                    padding:
-                    const EdgeInsets.only(left: 5.0, right: 5.0),
-                    child: Container(
-                      height: 80,
-                      padding: const EdgeInsets.all(8),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment:
-                        MainAxisAlignment.spaceBetween,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Container(
+                decoration: BoxDecoration(border: Border(top: BorderSide(width: 2,color: Colors.red))),
+                // color: Color(0xff1A202E),
+                child: Container(
+                  height: 80,
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment:
+                    MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment:
+                        CrossAxisAlignment.center,
                         children: <Widget>[
-                          Row(
+                          Container(
+                              height: 70,
+                              child: Padding(
+                                padding: const EdgeInsets.all(2.0),
+                                child: FadeInImage(
+                                  placeholder: const AssetImage('assets/image/cob.png'),
+                                  image: NetworkImage("$URL/Bitcoin/resources/icons/${list[i].name!.toLowerCase()}.png"),
+                                ),
+                              )
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 7, 0, 0),
+                                child: Text(
+                                  '${list[i].name}',
+                                  style: GoogleFonts.poppins(
+                                      textStyle:  const TextStyle(fontWeight: FontWeight.normal,fontSize: 24,color: Colors.white)
+                                  ),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          callCurrencyDetails(list[i].name);
+                        },
+                        child: Container(
+                          width:MediaQuery.of(context).size.width/4,
+                          height: 40,
+                          child: charts.LineChart(
+                            _createSampleData(list[i].historyRate, double.parse(list[i].diffRate!)),
+                            layoutConfig: charts.LayoutConfig(
+                                leftMarginSpec: charts.MarginSpec.fixedPixel(5),
+                                topMarginSpec: charts.MarginSpec.fixedPixel(10),
+                                rightMarginSpec: charts.MarginSpec.fixedPixel(5),
+                                bottomMarginSpec: charts.MarginSpec.fixedPixel(10)),
+                            defaultRenderer: charts.LineRendererConfig(includeArea: true, stacked: true,),
+                            animate: true,
+                            domainAxis: const charts.NumericAxisSpec(showAxisLine: false, renderSpec: charts.NoneRenderSpec()),
+                            primaryMeasureAxis: const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Column(
                             crossAxisAlignment:
-                            CrossAxisAlignment.center,
+                            CrossAxisAlignment.end,
                             children: <Widget>[
-                              SizedBox(
-                                  height: 70,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(2.0),
-                                    child: FadeInImage(
-                                      placeholder: const AssetImage('assets/image/cob.png'),
-                                      image: NetworkImage("$URL/Bitcoin/resources/icons/${list[i].name!.toLowerCase()}.png"),
-                                      // image: NetworkImage("http://45.34.15.25:8080/Bitcoin/resources/icons/${gainerLooserCoinList[i].name.toLowerCase()}.png"),
-                                    ),
-                                  )
-                              ),
-                              const SizedBox(
-                                width: 15,
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.fromLTRB(0, 7, 0, 0),
-                                    child: Text(
-                                      '${list[i].name}',
-                                      style: GoogleFonts.poppins(
-                                          textStyle:  const TextStyle(fontWeight: FontWeight.normal,fontSize: 24,color: Colors.black)
-                                      ),
-                                      textAlign: TextAlign.left,
-                                    ),
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    '\$${double.parse(list[i].rate!.toStringAsFixed(2))}',
+                                    style: GoogleFonts.poppins(textStyle: const TextStyle(fontSize: 15,color: Colors.white,   fontWeight: FontWeight.normal,)),
                                   ),
                                 ],
                               ),
-
-                            ],
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              callCurrencyDetails(list[i].name);
-                            },
-                            child: SizedBox(
-                              width:MediaQuery.of(context).size.width/4,
-                              height: 40,
-                              child: charts.LineChart(
-                                _createSampleData(list[i].historyRate, double.parse(list[i].diffRate!)),
-                                layoutConfig: charts.LayoutConfig(
-                                    leftMarginSpec: charts.MarginSpec.fixedPixel(5),
-                                    topMarginSpec: charts.MarginSpec.fixedPixel(10),
-                                    rightMarginSpec: charts.MarginSpec.fixedPixel(5),
-                                    bottomMarginSpec: charts.MarginSpec.fixedPixel(10)),
-                                defaultRenderer: charts.LineRendererConfig(includeArea: true, stacked: true,),
-                                animate: true,
-                                domainAxis: const charts.NumericAxisSpec(showAxisLine: false, renderSpec: charts.NoneRenderSpec()),
-                                primaryMeasureAxis: const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
-                              ),
-                            ),
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Column(
-                                crossAxisAlignment:
-                                CrossAxisAlignment.end,
-                                children: <Widget>[
-                                  Row(
-                                    children: <Widget>[
-                                      Text(
-                                        '\$${double.parse(list[i].rate!.toStringAsFixed(2))}',
-                                        style: GoogleFonts.poppins(textStyle: const TextStyle(fontSize: 15,color: Colors.white,   fontWeight: FontWeight.normal,)),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      double.parse(list[
-                                      i]
-                                          .diffRate!) <
-                                          0
-                                          ? const Icon(
-                                            Icons
-                                                .arrow_downward,
-                                            color: Colors.red,
-                                            size: 15,
-                                          )
-                                          : const Icon(
-                                            Icons.arrow_upward_sharp,
-                                            color: Colors.green,
-                                            size: 15,
-                                          ),
-                                      const SizedBox(
-                                          width:2
-                                      ),
-                                      Text(
-                                          list[i]
-                                              .perRate!,
-                                          style: GoogleFonts.poppins(
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.normal,
-                                              color: double.parse(
-                                                  list[
-                                                  i]
-                                                      .diffRate!) <
-                                                  0
-                                                  ? Colors.red
-                                                  : Colors.green)
-                                      ),
-                                    ],
+                              Row(
+                                children: [
+                                  double.parse(list[
+                                  i]
+                                      .diffRate!) <
+                                      0
+                                      ? Container(
+                                    // color: Colors.red,
+                                    child: const Icon(
+                                      Icons
+                                          .arrow_downward,
+                                      color: Colors.red,
+                                      size: 15,
+                                    ),
                                   )
+                                      : Container(
+                                    // color: Colors.green,
+                                    child: const Icon(
+                                      Icons.arrow_upward_sharp,
+                                      color: Colors.green,
+                                      size: 15,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                      width:2
+                                  ),
+                                  Text(
+                                      list[i]
+                                          .perRate!,
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.normal,
+                                          color: double.parse(
+                                              list[
+                                              i]
+                                                  .diffRate!) <
+                                              0
+                                              ? Colors.red
+                                              : Colors.green)
+                                  ),
                                 ],
                               )
                             ],
-                          ),
+                          )
                         ],
                       ),
-                    ),
+                    ],
                   ),
                 ),
-                const Divider(color: Colors.white,thickness: 1,endIndent: 20,indent: 20,),
-              ],
+              ),
             ),
             onTap: () {
               callCurrencyDetails(topLooserAndGainerList[i].name);
